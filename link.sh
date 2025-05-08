ee104b83235138814b025728ba4ced92ded32337
z0rhan <uknowme1928@gmail.com>
2025-05-08 20:22:45 +0300

    Add wezterm

--- link.sh
+++ link.sh
@@ -6,6 +6,7 @@ ln -sf $DOTFILES/nvim ~/.config/nvim
 ln -sf $DOTFILES/fastfetch ~/.config/fastfetch
 ln -sf $DOTFILES/rofi ~/.config/rofi
 ln -sf $DOTFILES/kitty ~/.config/kitty
+ln -sf $DOTFILES/wezterm ~/.config/wezterm
 ln -sf $DOTFILES/.tmux.conf ~/.tmux.conf
 ln -sf $DOTFILES/.vimrc ~/.vimrc
 
